018de99d8c953e481ea231b6f49b0db31eba01d3
swaraj ramesh malghe <swarajrmalghe@gmail.com>
2026-08-05 12:21:08 +0530

    Update A16Q3.py

--- assignment16/A16Q3.py
+++ assignment16/A16Q3.py
@@ -3,10 +3,11 @@ def Add(no1,no2):
 
 def main():
     num1 = int(input("Enter first number."))
+    
     num2 = int(input("Enter second number."))
 
     result = Add(num1,num2)
-    print("Addition is,",result)
+    print("Addition  is,", result)
 
 
 if __name__ == "__main__":
